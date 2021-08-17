0010f6ee23736ed93fec812dc807d0e53acf123f
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-08-17 11:41:20 +0800

    Update model.py

--- model.py
+++ model.py
@@ -152,7 +152,19 @@ def train_test(model, train_data, test_data):
     trainer.fit(model, train_loader)
     trainer.test(model, test_loader)
     
+class litdatamodule(LightningDataModule):
+    def __init__(self, batch_size):
+        super().__init__()
+        self.batch_size = batch_size
+    def prepare_data(self):
+        
+    def setup(self, stage=None):
+        
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(train_data, num_workers=4, batch_size=model.model.batch_size, shuffle=True, pin_memory=True)
 
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(train_data, num_workers=4, batch_size=model.model.batch_size, shuffle=False, pin_memory=True)
 class Litmodel(LightningModule):
     def __init__(self,model,opt):
         super().__init__()
