804b292b84e718310fcec3aa5f3afe66d118797f
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-09-01 10:44:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -49,17 +49,17 @@ def main():
 
     if opt.dataset == 'diginetica':
         num_node = 43098
-        #opt.n_iter = 1
+        opt.n_iter = 2
         opt.dropout_gcn = 0.2
         opt.dropout_local = 0.0
     elif opt.dataset == 'Nowplaying':
         num_node = 60417
-        #opt.n_iter = 2
+        opt.n_iter = 2
         #opt.dropout_gcn = 0.0
         opt.dropout_local = 0.0
     elif opt.dataset == 'Tmall':
         num_node = 40728
-        opt.n_iter = 1
+        opt.n_iter = 2
         opt.dropout_gcn = 0.6
         opt.dropout_local = 0.0
     else:
