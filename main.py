5b32a784ec7df87323a808a1100ae53efa371a26
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-09-10 17:47:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -49,17 +49,17 @@ def main():
         num_node = 43098
         opt.n_iter = 2
         opt.dropout_gcn = 0.2
-        opt.dropout_local = 0.0
+        #opt.dropout_local = 0.0
     elif opt.dataset == 'Nowplaying':
         num_node = 60417
         opt.n_iter = 1
         opt.dropout_gcn = 0.0
-        opt.dropout_local = 0.0
+        #opt.dropout_local = 0.0
     elif opt.dataset == 'Tmall':
         num_node = 40728
         opt.n_iter = 1
         opt.dropout_gcn = 0.6
-        opt.dropout_local = 0.5
+        #opt.dropout_local = 0.5
     elif opt.dataset == 'yoochoose1_64':
         num_node = 37484
         opt.n_iter = 1
